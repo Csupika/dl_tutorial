9ed84c9eeb66031f99acd9005e4c1ad380ceefef
PeterCseh <plc2000@hw.ac.uk>
2022-10-07 17:00:34 +0100

    Keras import change

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -11,7 +11,7 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import RMSprop
 
 batch_size = 128
 num_classes = 10
